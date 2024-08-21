ce109c6fa4d8bd2c084f56042bb32689bcc1b7f9
Andyka Setiabudi <andykasetiabudi@gmail.com>
2024-08-21 22:33:56 +0700

    add: res_review_upload and res_review_report locator

---
+++ elements/__res_evaluation_report_user.py
@@ -0,0 +1,21 @@
+class PageInfo:
+    url = 'https://staging.karirlab.co/dashboard/resume-review-report/[resume_report_id]'
+    back_action = "#backToDashboardResume"
+    title = "//h1[contains(@class, 'resumeReviewReportScreen__title')]"
+
+
+# class ResumeSummary:
+
+# class ResumeScores:
+
+# class ResumeReportFormats:
+
+# class ResumeReportSelfInfo:
+
+# class ResumeReportEducation:
+
+# class ResumeReportOccupation: 
+
+# class ResumeReportProficiency:
+
+# class ResumeReportAdditionalInfo:
